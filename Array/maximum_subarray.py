d32aeaf45319bfab5b23e895a9af282c94aee9f9
harshu <pujariharshu99@gmail.com>
2025-10-05 22:58:40 +0530

    updated easy approach using kadane algorithm.

--- Array/maximum_subarray.py
+++ Array/maximum_subarray.py
@@ -1,21 +1,14 @@
 from typing import List
 
-
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        # Initialize current and global maximum
-        max_current = max_global = nums[0]
+    def maxSubArray(self, arr: List[int]) -> int:
+        curr_sum = max_sum = arr[0]  
 
-        # Loop through array starting from the 2nd element
-        for i in range(1, len(nums)):
-            # Update current max (either start new subarray or add to existing one)
-            max_current = max(nums[i], max_current + nums[i])
+        for i in range(1, len(arr)):
+            curr_sum = max(arr[i], curr_sum + arr[i])  
+            max_sum = max(max_sum, curr_sum)           
 
-            # Update global max if needed
-            max_global = max(max_global, max_current)
+        return max_sum
 
-        return max_global
-
-
-nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(Solution().maxSubArray(nums))
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(Solution().maxSubArray(arr))   
