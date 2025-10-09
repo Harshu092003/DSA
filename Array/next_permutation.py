791972b08de3e480d0227bae25647c523b78a82b
Harshvardhan Pujari <harshvardhan_pujari@mail.mumbai-tech.com>
2025-10-09 22:53:37 +0530

    binary exponentiation log n approach, and next permutation

---
+++ Array/next_permutation.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        # S Find first decreasing element from the right
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        #  If found, find the next bigger element on right side
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            # Swap them
+            nums[i], nums[j] = nums[j], nums[i]
+
+        #  Reverse the remaining right half
+        nums[i + 1:] = reversed(nums[i + 1:])
+
+
+nums = [1, 2, 3]
+Solution().nextPermutation(nums)
+print(nums)
