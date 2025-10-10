257d34867f8d65d4043b1f268b61705636c22d1b
Harshvardhan Pujari <harshvardhan_pujari@mail.mumbai-tech.com>
2025-10-11 00:19:32 +0530

    buying first stock at day which minimum ,then selling it on maximum profit day

---
+++ Array/best_time_buy&sell_stock.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices : list[int]) -> int :
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            # Find the smallest price so far
+            if price < min_price:
+                min_price = price
+            # Calculate profit if we sell today
+            profit = price - min_price
+            # Update max profit
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+print(Solution().maxProfit(prices))
