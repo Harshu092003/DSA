82e5681e9b9d551e4332d0530465fae5e680b832
Harshvardhan Pujari <harshvardhan_pujari@mail.mumbai-tech.com>
2025-10-25 15:06:39 +0530

    solved valid anagram using sorting

---
+++ String/valid_anagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def valid_anagram(self,s : str, t : str) -> bool :
+        print(sorted(s))
+        print(sorted(t))
+        return sorted(s) == sorted(t)
+    
+print(Solution().valid_anagram("nagaram","anagram"))
