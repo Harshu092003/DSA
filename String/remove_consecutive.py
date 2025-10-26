91f1fbdb4a863c4432c65e83e1b076ba984f0491
Harshvardhan Pujari <harshvardhan_pujari@mail.mumbai-tech.com>
2025-10-26 14:24:17 +0530

    removing duplicatings and adding only one characters if its duplicate else its will add other characters

---
+++ String/remove_consecutive.py
@@ -0,0 +1,13 @@
+class Solution():
+    def remove_consecutive(self , s : str) -> str :
+        result = ""
+        for i in range(len(s)):
+            if i == len(s) -1 or s[i] != s[i+1] :
+                result += s[i]
+        return result
+
+s = "aabb"
+print(Solution().remove_consecutive(s))
+
+s = "aabaa"
+print(Solution().remove_consecutive(s))
