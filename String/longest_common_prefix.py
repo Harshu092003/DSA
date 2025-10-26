00767e2fae0caea0b99b988ca7c886bbe1956c6f
Harshvardhan Pujari <harshvardhan_pujari@mail.mumbai-tech.com>
2025-10-26 14:46:18 +0530

    finding common prefix in list of array

---
+++ String/longest_common_prefix.py
@@ -0,0 +1,13 @@
+class Solution :
+    def common_prefix(self,s : list[str]) -> str :
+        for i in range(len(s[0])):
+            for word in s[1:]:
+                if i >= len(word) or word[i] != s[0][i] :
+                    return s[0][:i]
+
+        return s[0]
+    
+s = ["flower","flow","flight"]
+print(Solution().common_prefix(s))
+
+print(Solution().common_prefix(["dog", "racecar", "car"])) 
