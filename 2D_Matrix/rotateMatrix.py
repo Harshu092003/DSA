b15c2f56b91dd5dd49532e5857453b9ee340c23c
Harshvardhan pujari <harshvardhan_pujari@mail.mumbai-tech.com>
2025-12-04 22:46:49 +0530

    tranposed the matrix

---
+++ 2D_Matrix/rotateMatrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> list[list[int]]:
+        n = len(matrix)
+        
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # Reverse each row
+        for i in range(n):
+            matrix[i].reverse()
+            
+        for row in matrix:
+            print(row)
+        
+        return matrix 
+    
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],  
+    [7, 8, 9]
+]
+print(Solution().rotate(matrix))
